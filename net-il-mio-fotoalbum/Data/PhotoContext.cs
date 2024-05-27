9149da1745e8d3fec91da0dd13a64bd4a3280805
Samuele-Benato <samuele.benato@gmail.com>
2024-05-27 12:41:10 +0200

    Add project files.

---
+++ net-il-mio-fotoalbum/Data/PhotoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Data
+{
+    public class PhotoContext : DbContext  
+    {
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=db-photo-libraries;");
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,24 @@
+﻿namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[]? ImageFile { get; set; }
+        public string? ImgSrc => ImageFile != null ? $"data:image/png;base64,{Convert.ToBase64String(ImageFile)}" : "";
+        public bool Visible {  get; set; }
+        public List<Category>? Categories { get; set; }
+
+        public Photo() { }
+        public Photo(int id, string title, string description, byte[]? imageFile, bool visible, List<Category>? categories)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            ImageFile = imageFile;
+            Visible = visible;
+            Categories = categories;
+        }
+    }
+}
